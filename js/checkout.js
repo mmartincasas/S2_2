d9c0a0c761bda333aa64c45b63dee7dde47fb4c4
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2024-07-23 10:26:34 +0200

    Añadido images y cambios de css

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,22 @@
 
-// Exercise 6
+
+
 function validate() {
 	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	
+	let fName = document.getElementById("fName");
+	let fEmail = document.getElementById("fEmail");
+	let fAddress = document.getElementById("fAddress");
+	let fLastN = document.getElementById("fLastN");
+	let fPassword = document.getElementById("fPassword");
+	let fPhone = document.getElementById("fPhone");
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
+	let errorName = document.getElementById("errorName");
+	let errorEmail = document.getElementById("errorEmail");
+	let errorAddress = document.getElementById("errorAddress");  
+	let errorLastN = document.getElementById("errorLastN");  
+	let errorPassword = document.getElementById("errorPassword");  
+	let errorPhone = document.getElementById("errorPhone");    
 	
 	// Validate fields entered by the user: name, phone, password, and email
 	if(fName.value == ""){
@@ -18,6 +26,12 @@ function validate() {
 	if(fEmail.value == ""){
 		error++;
 	}
+
+    // Valida el campo de email
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(fEmail.value)) {
+        showError(fEmail, errorEmail);
+        valid = false;
+    }
 	 
 	if(error>0){
 		alert("Error");
@@ -26,3 +40,28 @@ function validate() {
 	}
 
 }
+
+
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function validate2 () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+        })
+    })()
